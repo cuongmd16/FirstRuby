0146fd8807379aad3e97c54ea538f85f4262b1d4
CuongDM <cuongdm162000@gmail.com>
2021-08-23 09:33:46 +0700

    cap nhat lan 3

--- ptbac2.rb
+++ ptbac2.rb
@@ -1,28 +1,29 @@
-puts "Nhap A:"
-	a = gets()
-	a = a.to_i
-puts "Nhap B:"
-	b = gets()
-	b= b.to_i
-puts "Nhap C:"
-	c = gets()
-	c = c.to_i
-
-if a != 0 then
-  delta = (b*b) - (4*a*c)
-  sqrt_delta = CMath.sqrt(delta)
-  if delta > 0 then
-    x1 = (-b + sqrt_delta)/(a*2)
-    x2 = (-b - sqrt_delta)/(a*2)
-    puts "x: #{x} - x1: #{x1}"
-  else
-    if delta == 0 then
-      x = -b/(a*2)
-      puts "Phuong trinh co nghiem kep x=x1=: #{x2}"
+require "cmath"
+class Input
+  attr_accessor :a, :b, :c
+  
+  def self.Giaipt(a,b,c)
+    @a = a.to_i
+    @b = b.to_i
+    @c = c.to_i
+    if a != 0 then
+      delta = (b*b) - (4*a*c)
+      sqrt_delta = CMath.sqrt(delta)
+      if delta > 0 then
+        x1 = (-b + sqrt_delta)/(a*2)
+        x2 = (-b - sqrt_delta)/(a*2)
+        puts "x1: #{x1} - x2: #{x2}"
+      else
+        if delta == 0 then
+          x = -b/(a*2)
+          puts "Phuong trinh co nghiem kep x=x1=: #{x}"
+        else
+          puts "Phuong trinh vo nghiem"
+        end
+      end
     else
-      puts "Phuong trinh vo nghiem"
+      puts "Day ko phai phuong trinh bac 2"
     end
   end
-else
-  puts "Day ko phai phuong trinh bac 2"
 end
+
