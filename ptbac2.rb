ab5ce40747eeaa2a4a95f257de56e57ae2e347b7
CuongDM <cuongdm162000@gmail.com>
2021-08-24 16:01:46 +0700

    Fix lan 4

--- ptbac2.rb
+++ ptbac2.rb
@@ -3,63 +3,55 @@ require "cmath"
 class PTB2
 
   attr_reader :a, :b, :c
+  attr_accessor :n
 
   def initialize(a,b,c)
-    @a=a
-    @b=b
-    @c=c
+    @a = a
+    @b = b
+    @c = c
   end
 	
-    @n=-1
+  $n = nil
   def giai
+    @n = 0
     delta = send :delta1
-    if @a != 0
-    @n=-1
+    if a != 0
       if delta  > 0
-      	@n=1
-        @x1 = (-@b + Math.sqrt(delta))/(@a*2)
-        @x2 = (-@b - Math.sqrt(delta))/(@a*2)
+        @x1 = (-b + Math.sqrt(delta))/(a*2)
+        @x2 = (-b - Math.sqrt(delta))/(a*2)
       elsif delta == 0
-      	@n=0
-        @x = -@b/(@a*2)
+      	@n = 1
+        @x = -b/(a*2)
+      else
+        @n = 2
       end
     else
-      @n=5 
+      @n = 3
     end
   end
 
   def ket_qua
-	if (@x1 == nil) && (@x == nil) 
-		puts "Bạn chưa thực hiện giải phương trình, vui lòng call method giai_phuong_trinh trước"
-	elsif @n==1  
-	    puts "x1: #{@x1} - x2: #{@x2}"
-		elsif @n==0 
-		    puts "PT nghiệm kép x1 = x2 = : #{@x}"
-		    elsif @n==-1
-			 puts "vo nghiem"
-				elsif @n==5
-		 		puts "Day khong phai pt bac 2"
-				 
-	end
+		if n.nil?
+      puts "Bạn chưa thực hiện giải phương trình, vui lòng call method giai_phuong_trinh trước"
+    elsif n == 0
+      puts "Pt co 2 nghiem la x1: #{@x1} - x2: #{@x2}"
+	  elsif n == 1
+      puts "Nghiem kep x = #{@x}"
+    elsif n == 2
+      puts "PT vo nghiem"
+    else
+      puts "Non pt2"
+    end
   end
-
   
   private
   def delta1
-    @delta = (@b*@b) - (4*@a*@c)
+    @delta = (b*b) - (4*a*c)
   end
-
-  # def output(result)
-  #   if result > 0 
-  #     puts "x1: #{@x1} - x2: #{@x2}"
-  #   elsif result == 0
-  #     puts "Nghiem kep cua PT: #{@x1}"
-  #   else
-  #     puts "Phuong trinh vo nghiem"
-  #   end
-  # end
 end
 
-ptb2 = PTB2.new(1,5,-3)
+ptb2 = PTB2.new(1,2,6)
 ptb2.giai
 ptb2.ket_qua
+
+
