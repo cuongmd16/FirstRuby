c4a6bafb5fe3cabadc26868e884a97e8f2731313
CuongDM <cuongdm162000@gmail.com>
2021-08-25 10:52:53 +0700

    lan5

--- ptbac2.rb
+++ ptbac2.rb
@@ -1,7 +1,7 @@
 require "cmath"
 
 class PTB2
-
+  
   attr_reader :a, :b, :c
   attr_accessor :n
 
@@ -11,47 +11,50 @@ class PTB2
     @c = c
   end
 	
-  $n = nil
+  # $n = nil
   def giai
-    @n = 0
     delta = send :delta1
-    if a != 0
+   
+      if delta == nil
+        return @n = "khong phai pt bac 2"
+      end
       if delta  > 0
         @x1 = (-b + Math.sqrt(delta))/(a*2)
         @x2 = (-b - Math.sqrt(delta))/(a*2)
-      elsif delta == 0
-      	@n = 1
-        @x = -b/(a*2)
-      else
-        @n = 2
+        @n = "x1: #{@x1} -  x2: #{@x2}"
+        return n
       end
-    else
-      @n = 3
-    end
+      if delta == 0
+        @x = -b/(a*2)
+        @n = "pt co nghiem kep x1=x2 = #{@x}"
+        return n
+      end 
+        @n = "pt vo nghiem"
   end
-
+    
   def ket_qua
-		if n.nil?
+    if n == nil 
       puts "Bạn chưa thực hiện giải phương trình, vui lòng call method giai_phuong_trinh trước"
-    elsif n == 0
-      puts "Pt co 2 nghiem la x1: #{@x1} - x2: #{@x2}"
-	  elsif n == 1
-      puts "Nghiem kep x = #{@x}"
-    elsif n == 2
-      puts "PT vo nghiem"
-    else
-      puts "Non pt2"
+      return
     end
+    puts "#{@n}"
   end
   
   private
+
+  
+
   def delta1
-    @delta = (b*b) - (4*a*c)
+    if a != 0
+    delta = (b*b) - (4*a*c)
+    return delta
+    end
+    delta = nil
   end
 end
 
-ptb2 = PTB2.new(1,2,6)
-ptb2.giai
+ptb2 = PTB2.new(1,-5,3)
+#ptb2.giai
 ptb2.ket_qua
 
 
